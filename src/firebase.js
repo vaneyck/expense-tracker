de0444073150ff6661318b6727479d83f5fe8773
Vaneyck <vaneycksemeotoki@gmail.com>
2019-10-15 15:11:22 +0300

    Can pull messageToken

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,14 @@ import firebaseAPI from 'firebase'
 import * as firebaseui from "firebaseui"
 require('firebase/firestore')
 
-var config = {
+const config = {
   apiKey: "AIzaSyC6c2gfMTNF-1gEbnXh8CvnXx8HTl7Deps",
   authDomain: "expense-tracker-ac6d8.firebaseapp.com",
   databaseURL: "https://expense-tracker-ac6d8.firebaseio.com",
   projectId: "expense-tracker-ac6d8",
   storageBucket: "expense-tracker-ac6d8.appspot.com",
-  messagingSenderId: "211067149885"
+  messagingSenderId: "211067149885",
+  appId: "1:211067149885:web:ebd906e225aba5a4"
 };
 
 // Initiailize the firebase App
@@ -40,6 +41,40 @@ firestoreDatabase.enablePersistence()
     }
   });
 
+// Firebase messaging
+const messagingApi = firebaseAPI.messaging();
+messagingApi.usePublicVapidKey("BPcBcrqojNuTKM2Ar1aZU9rr31X2nctN0e_ccCgYfSNQlwVTPJEC7eRDAYhQJ2kPjVdeMb1BR0A2WC9A6grej08");
+
+Notification.requestPermission().then((permission) => {
+  if (permission === 'granted') {
+    console.log('Notification permission granted.');
+  } else {
+    console.log('Unable to get permission to notify.');
+  }
+});
+
+messagingApi.getToken().then((currentToken) => {
+  if (currentToken) {
+    console.log("Current Token")
+  } else {
+    // Show permission request.
+    console.log('No Instance ID token available. Request permission to generate one.');
+  }
+}).catch((err) => {
+  console.log('An error occurred while retrieving token. ', err);
+});
+
+// Callback fired if Instance ID token is updated.
+messagingApi.onTokenRefresh(() => {
+  messaging.getToken().then((refreshedToken) => {
+    console.log('Token refreshed.', refreshedToken);
+    // ...
+  }).catch((err) => {
+    console.log('Unable to retrieve refreshed token ', err);
+  });
+});
+
 export const firebase = firebaseAPI
 export const signInUi = ui
 export const firestore = firestoreDatabase
+export const messaging = messagingApi
